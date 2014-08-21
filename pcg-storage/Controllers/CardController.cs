b40abec4e3ca90a29b45f959fc4277e6eff640c6
mochablendy <mochablendy at gmail dot com>
2014-08-21 15:35:35 +0900

    [enable] SQL Server CE

---
+++ pcg-storage/Controllers/CardController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using pcg_storage.Models;
+
+namespace pcg_storage.Controllers
+{
+    public class CardController : Controller
+    {
+        private PCGStorageContext db = new PCGStorageContext();
+
+        // GET: Card
+        public ActionResult Index()
+        {
+            return View(db.Cards.ToList());
+        }
+
+        // GET: Card/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // GET: Card/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Card/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Type")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cards.Add(card);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(card);
+        }
+
+        // GET: Card/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Card/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、http://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Type")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(card).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(card);
+        }
+
+        // GET: Card/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = db.Cards.Find(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Card/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Card card = db.Cards.Find(id);
+            db.Cards.Remove(card);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
